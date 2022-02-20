4804ad26d96b17dcda08878574edd818d8d5c9c6
Yannick Schäfer <mail@m00n.dev>
2022-02-20 22:44:49 +0100

    chore: Remove commented out code

--- src/validator.ts
+++ src/validator.ts
@@ -1,13 +1,11 @@
 import 'reflect-metadata';
 
-import ErrorStackParser from 'error-stack-parser';
 import { ClassDeclaration, Project } from 'ts-morph';
 
 import { ClassDiscovery } from './class-discovery';
 import { flattenValidationError, IErrorMessage } from './error-formatter';
-import { ClassNotDecoratedError, ClassNotFoundError } from './errors';
 import { INodeValidationError, ITypeAndTree, RootNode } from './nodes';
-import { ClassCache, isClass, Parser } from './parser';
+import { Parser } from './parser';
 import { IClassMeta, SourceCodeLocationDecorator } from './source-code-location-decorator';
 import { Constructor } from './types';
 
@@ -19,14 +17,6 @@ interface IValidatorOptions {}
 export interface IValidationOptions {
   allowUnknownFields?: boolean;
 }
-
-// export interface IValidatorClassMeta {
-//   filename: string;
-//   line: number;
-
-//   options: IValidatorOptions;
-// }
-
 export interface IValidationSuccess<T> {
   success: true;
   object: T;
@@ -53,125 +43,6 @@ export interface IValidatorConstructorOptions {
   project: Project;
   defaultOptions?: IValidationOptions;
 }
-
-// export class ClassDiscovery {
-//   project: Project;
-//   classCache = new Map<string, ClassDeclaration>(); // TODO: Better name
-
-//   constructor(project: Project) {
-//     this.project = project;
-//   }
-
-//   /**
-//    * This method tries to find a class based on the className, filename and line where the @Validator() call occured.
-//    *
-//    * This loops through the whole syntax tree of the source file, and checks each class syntax node
-//    * if the line correspondents to the class. Because this is rather hacky, the `line` parameter
-//    * might not line up exactly to the class syntax node's start line. This considers decorators,
-//    * but will fail for more unconventional code styles. This however should always work:
-//    *
-//    * ```
-//    * @SomeDecorator()
-//    * @Validator()
-//    * @SomeOtherDecorator()
-//    * class Test {}
-//    *
-//    * ```
-//    *
-//    * @param filename Filename of the file the class should be searched
-//    * @param className String representation of the className
-//    * @param line The line where the @Decorator() call occurred
-//    * @returns
-//    */
-//   getClass(className: string, filename: string, line: number): ClassDeclaration {
-//     const cacheKey = `${line}:${filename}`;
-//     const cached = this.classCache.get(cacheKey);
-//     if (cached) {
-//       return cached;
-//     }
-
-//     const sourceFile = this.project.getSourceFileOrThrow(filename);
-
-//     const walkSyntaxNodes = (node: Node, level = 0): ClassDeclaration | null => {
-//       for (const childNode of node.getChildren()) {
-//         if (isClass(childNode)) {
-//           const cls = childNode;
-
-//           // A class can have multiple decorators, collect their line numbers and check them too
-//           const decoratorLineStarts = cls.getDecorators().map((decorator) => decorator.getStartLineNumber());
-//           for (const decoratorLineStart of decoratorLineStarts) {
-//             if (decoratorLineStart === line) {
-//               return cls;
-//             }
-//           }
-
-//           if (cls.getStartLineNumber() === line) {
-//             return cls;
-//           }
-//         }
-//         const cls = walkSyntaxNodes(childNode, level + 1);
-//         if (cls) {
-//           return cls;
-//         }
-//       }
-//       return null;
-//     };
-
-//     const cls = walkSyntaxNodes(sourceFile);
-
-//     if (!cls) {
-//       throw new ClassNotFoundError(`Class not found: ${className}`);
-//     }
-
-//     this.classCache.set(cacheKey, cls);
-
-//     return cls;
-//   }
-// }
-
-// export class SourceCodeLocationDecorator<T> {
-//   classDiscovery: ClassDiscovery;
-//   symbol: symbol;
-
-//   classDeclarationToClassReference = new ClassCache<Constructor<unknown>>();
-
-//   constructor(classDiscovery: ClassDiscovery) {
-//     this.classDiscovery = classDiscovery;
-//     this.symbol = Symbol(`SourceCodeLocationDecorator${new Date().getTime()}`);
-//   }
-
-//   decorator(options: T = {} as T, error: Error): ReturnType<typeof Reflect['metadata']> {
-//     const stack = ErrorStackParser.parse(error);
-
-//     const filename = stack[1].fileName!;
-//     const line = stack[1].lineNumber!;
-
-//     const classMetadata = {
-//       filename,
-//       line,
-//       options,
-//     };
-
-//     return (target: object) => {
-//       const classDeclaration = this.classDiscovery.getClass(target.constructor.name, filename, line);
-//       this.classDeclarationToClassReference.set(classDeclaration, target as Constructor<unknown>);
-//       Reflect.defineMetadata(this.symbol, classMetadata, target);
-//     };
-//   }
-
-//   getClassMetadata(cls: Constructor<unknown>): IClassMeta<T> {
-//     const meta = Reflect.getMetadata(this.symbol, cls);
-//     if (meta?.filename && meta?.line && meta?.options) {
-//       return meta as IClassMeta<T>;
-//     } else {
-//       throw new ClassNotDecoratedError('No class metadata found', { cls: cls.name });
-//     }
-//   }
-
-//   getClassDeclarationMapping(): ClassCache<Constructor<unknown>> {
-//     return this.classDeclarationToClassReference;
-//   }
-// }
 
 export class ValidatorInstance {
   project: Project;
@@ -212,8 +83,8 @@ export class ValidatorInstance {
    * This loops through all direct and indirect properties of `cls` and outputs them in the internal
    * TypeNode tree format
    *
-   * @param cls Class reference
-   * @param classDeclaration A ts-morph class declaration whose members will be processed
+   * @param cls - Class reference
+   * @param classDeclaration - A ts-morph class declaration whose members will be processed
    */
   getPropertyTypeTrees<T>(cls: Constructor<T>, classDeclaration: ClassDeclaration): ITypeAndTree[] {
     const trees = this.parser.getPropertyTypeTrees(classDeclaration);
