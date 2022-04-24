3f16f3d6462d1a054a6a949c5a684749bffaf2bb
Yannick Schäfer <mail@m00n.dev>
2022-04-25 00:58:16 +0200

    feat: Add commitlint pipeline step

---
+++ commitlint.config.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [2, 'always', 'sentence-case'],
+  },
+};
