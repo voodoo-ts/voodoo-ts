c96d057683b717031445ec83ac01430e6d645302
Yannick Schäfer <mail@m00n.dev>
2022-04-25 02:35:16 +0200

    feat: Use JSON config for commitlintrc

--- commitlint.config.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'subject-case': [2, 'always', 'sentence-case'],
-  },
-};
