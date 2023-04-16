82716a263c1190d5f395a3f9f50a094c1e00965d
Yannick Schäfer <mail@m00n.dev>
2023-04-16 19:27:26 +0200

    chore(release): make microrelease configurable

--- src/scripts/release.ts
+++ src/scripts/release.ts
@@ -3,6 +3,11 @@ import { execSync } from 'node:child_process';
 import { readFileSync } from 'node:fs';
 import * as readlineModule from 'node:readline';
 
+interface Settings {
+  allowedBranch?: string;
+  message?: string;
+}
+
 const readline = readlineModule.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -44,13 +49,38 @@ function getCommitTypes(from: string, to: string): Map<string, number> {
 }
 
 function main(): void {
-  const { version: currentVersion } = JSON.parse(readFileSync('package.json').toString('utf8')) as { version: string };
+  const { version: currentVersion, microrelease: packageSettings } = JSON.parse(
+    readFileSync('package.json').toString('utf8'),
+  ) as {
+    version: string;
+    microrelease?: Settings;
+  };
+
+  const settings = Object.assign(
+    {
+      message: 'chore(package): bump version to %s',
+    },
+    packageSettings,
+  );
+
+  console.log(`🎫 package.json loaded, config:`);
+
+  for (const [key, value] of Object.entries(settings)) {
+    console.log(`   ${key}: ${value}`);
+  }
+
+  const branch = execSync(`git branch --show-current`).toString().trim();
+  if (settings.allowedBranch && branch !== settings.allowedBranch) {
+    console.log(`❌ Branch must be ${settings.allowedBranch}, current branch is ${branch}`);
+    process.exit(1);
+  }
+  console.log(`🌳 Branch is ${branch}`);
 
   console.log(`👉 Current version: ${currentVersion}`);
   const tag = execSync(`git tag -l v${currentVersion}`).toString();
   if (!tag) {
     console.log(`❌ Tag "v${currentVersion}" for version not found`);
-    process.exit(1);
+    process.exit(2);
   } else {
     console.log('✅ Tag found');
   }
@@ -60,17 +90,17 @@ function main(): void {
 
   if (!types.size) {
     console.log(`❌ No commits found`);
-    process.exit(1);
+    process.exit(3);
   }
 
   const bumpMinorVersion = types.has('feat') || types.has('refactor');
   const versionCommandParam = bumpMinorVersion ? 'minor' : 'patch';
-  const versionCommand = `npm version ${versionCommandParam} -m "chore(package): bump version to %s"`;
+  const versionCommand = `npm version ${versionCommandParam} -m "${settings.message}"`;
 
   readline.question(`❔ Will run "${versionCommand}"`, () => {
-    console.log(`🚀 Running npm version. Good luck!`);
-    const result = execSync(versionCommand);
-    console.log(result.toString());
+    console.log(`🍀 Running npm version. Good luck!`);
+    const result = execSync(versionCommand).toString().trim();
+    console.log(`🚀 New version: ${result}`);
 
     readline.close();
   });
