9f97935f5579ac58e482f94d58b74fff6a38783e
Yannick Schäfer <mail@m00n.dev>
2023-04-30 23:33:20 +0200

    refactor: split TransformerInstance into a base class and concrete class

--- src/transformer.ts
+++ src/transformer.ts
@@ -2,8 +2,12 @@ import { Project } from 'ts-morph';
 
 import { ClassDiscovery } from './class-discovery';
 import { FormattedErrors, formatErrors } from './error-formatter';
-import { IValidationOptions, ITypeAndTree, ClassNode, INodeValidationError } from './nodes';
-import { SourceCodeLocationDecorator, IClassMeta } from './source-code-location-decorator';
+import { IValidationOptions, ITypeAndTree, ClassNode, INodeValidationError, INodeValidationResult } from './nodes';
+import {
+  SourceCodeLocationDecorator,
+  BasicSourceCodeLocationDecorator,
+  IClassMeta,
+} from './source-code-location-decorator';
 import {
   AbstractValueTransformerFactory,
   defaultFactory,
@@ -12,9 +16,9 @@ import {
   TransformerParser,
 } from './transformer-parser';
 import { Constructor } from './types';
-import { ClassCache, Parser } from './validator-parser';
+import { Parser, TypeCache } from './validator-parser';
 
-interface ITransformerOptions extends IValidatorOptions {
+export interface ITransformerOptions extends IValidatorOptions {
   cls?: Constructor<unknown>;
   factory?: Factory<unknown>;
 }
@@ -48,7 +52,7 @@ export type MaybePartial<T> = Partial<T> & Record<any, any>;
 export interface IValidatorConstructorOptions {
   project: Project;
   defaultOptions?: IValidationOptions;
-  parser?: (classDeclarationMapping: ClassCache<Constructor<unknown>>) => Parser;
+  parser?: (classDeclarationMapping: TypeCache<Constructor<unknown>>) => Parser;
   classDiscovery?: ClassDiscovery;
   decorator?: SourceCodeLocationDecorator<IValidatorOptions>;
 }
@@ -66,7 +70,102 @@ export class ValidationError extends Error {
 
 const NODE_MODULE_PATH = './node_modules/@vvalidator/vvalidator/src/*';
 
-export class TransformerInstance {
+export abstract class BaseTransformerInstance {
+  abstract transformerClassDecoratorFactory: BasicSourceCodeLocationDecorator<ITransformerOptions>;
+
+  abstract transform<T>(
+    cls: Constructor<T>,
+    values: MaybePartial<T>,
+    options?: ITransformationOptions,
+  ): Promise<IValidationResult<T> & { object: unknown }>;
+
+  abstract validate<T>(
+    cls: Constructor<T>,
+    values: MaybePartial<T>,
+    options?: IValidationOptions,
+  ): IValidationResult<T>;
+  abstract getClassNode<T>(cls: Constructor<T>): ClassNode;
+
+  transformerDecorator(options: ITransformerOptions = {}): ReturnType<(typeof Reflect)['metadata']> {
+    return this.transformerClassDecoratorFactory.decorator(new Error(), options);
+  }
+
+  getClassMetadata(cls: Constructor<unknown>): IClassMeta<ITransformerOptions> {
+    return this.transformerClassDecoratorFactory.getClassMetadata(cls);
+  }
+
+  nodeValidationResultToValidationResult<T>(result: INodeValidationResult): IValidationResult<T> {
+    if (result.success) {
+      return {
+        success: true,
+        object: result.value as T,
+      };
+    } else {
+      return {
+        success: false,
+        rawErrors: result,
+        object: null,
+        errors: formatErrors(result),
+      };
+    }
+  }
+
+  getFactory(cls: Constructor<unknown>): Factory<unknown> {
+    const validatorMeta = this.getClassMetadata(cls);
+    if (validatorMeta.options.factory) {
+      return validatorMeta.options.factory;
+    } else if (validatorMeta.options.cls) {
+      return defaultFactory(validatorMeta.options.cls);
+    } else {
+      return defaultFactory(cls);
+    }
+  }
+
+  validateOrThrow<T>(cls: Constructor<T>, values: MaybePartial<T>, options: IValidationOptions = {}): T {
+    const result = this.validate(cls, values, options);
+    if (result.success) {
+      return result.object;
+    } else {
+      throw new ValidationError(result.rawErrors, formatErrors(result.rawErrors));
+    }
+  }
+
+  async transformOrThrow<T>(
+    cls: Constructor<T>,
+    values: MaybePartial<T>,
+    options: ITransformationOptions = {},
+  ): Promise<T> {
+    const result = await this.transform(cls, values, options);
+    if (result.success) {
+      return result.object;
+    } else {
+      throw new ValidationError(result.rawErrors, formatErrors(result.rawErrors));
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  unwrap(): {
+    transformer: BaseTransformerInstance;
+    validate: BaseTransformerInstance['validate'];
+    validateOrThrow: BaseTransformerInstance['validateOrThrow'];
+    transform: BaseTransformerInstance['transform'];
+    transformOrThrow: BaseTransformerInstance['transformOrThrow'];
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    Dto: BaseTransformerInstance['transformerDecorator'];
+  } {
+    return {
+      transformer: this,
+      validate: this.validate.bind(this),
+      validateOrThrow: this.validateOrThrow.bind(this),
+      transform: this.transform.bind(this),
+      transformOrThrow: this.transformOrThrow.bind(this),
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      Dto: this.transformerDecorator.bind(this),
+    };
+  }
+}
+
+export class TransformerInstance extends BaseTransformerInstance {
   project: Project;
 
   parser: TransformerParser;
@@ -76,6 +175,8 @@ export class TransformerInstance {
   defaultOptions: IValidationOptions;
 
   constructor(options: ITransformerConstructorOptions) {
+    super();
+
     this.project = options.project;
     this.classDiscovery = new ClassDiscovery(options.project);
 
@@ -108,26 +209,6 @@ export class TransformerInstance {
     project.addSourceFilesAtPaths(NODE_MODULE_PATH);
 
     return new TransformerInstance({ project, ...options });
-  }
-
-  getFactory(cls: Constructor<unknown>): Factory<unknown> {
-    const validatorMeta = this.getClassMetadata(cls);
-    if (validatorMeta.options.factory) {
-      return validatorMeta.options.factory;
-    } else if (validatorMeta.options.cls) {
-      return defaultFactory(validatorMeta.options.cls);
-    } else {
-      return defaultFactory(cls);
-    }
-  }
-
-  async transformOrThrow<T>(cls: Constructor<T>, values: MaybePartial<T>): Promise<T> {
-    const result = await this.transform(cls, values);
-    if (result.success) {
-      return result.object;
-    } else {
-      throw new ValidationError(result.rawErrors, formatErrors(result.rawErrors));
-    }
   }
 
   getClassNode<T>(cls: Constructor<T>): ClassNode {
@@ -169,15 +250,6 @@ export class TransformerInstance {
         object: null,
         errors: formatErrors(result),
       };
-    }
-  }
-
-  validateOrThrow<T>(cls: Constructor<T>, values: MaybePartial<T>, options: IValidationOptions = {}): T {
-    const result = this.validate(cls, values, options);
-    if (result.success) {
-      return result.object;
-    } else {
-      throw new ValidationError(result.rawErrors, formatErrors(result.rawErrors));
     }
   }
 
@@ -223,34 +295,5 @@ export class TransformerInstance {
       validatorMeta.column,
     );
     return this.parser.getPropertyTypeTrees(classDeclaration);
-  }
-
-  transformerDecorator(options: ITransformerOptions = {}): ReturnType<(typeof Reflect)['metadata']> {
-    return this.transformerClassDecoratorFactory.decorator(new Error(), options);
-  }
-
-  getClassMetadata(cls: Constructor<unknown>): IClassMeta<ITransformerOptions> {
-    return this.transformerClassDecoratorFactory.getClassMetadata(cls);
-  }
-
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  unwrap(): {
-    transformer: TransformerInstance;
-    validate: TransformerInstance['validate'];
-    validateOrThrow: TransformerInstance['validateOrThrow'];
-    transform: TransformerInstance['transform'];
-    transformOrThrow: TransformerInstance['transformOrThrow'];
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    Dto: TransformerInstance['transformerDecorator'];
-  } {
-    return {
-      transformer: this,
-      validate: this.validate.bind(this),
-      validateOrThrow: this.validateOrThrow.bind(this),
-      transform: this.transform.bind(this),
-      transformOrThrow: this.transformOrThrow.bind(this),
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      Dto: this.transformerDecorator.bind(this),
-    };
   }
 }
